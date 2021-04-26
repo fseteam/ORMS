52906fcc74ade9ccd4c33de71955b31ad4ab3deb
abdimak <abdirohman567024@gmail.com>
2021-04-26 13:58:42 -0800

    edited

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
   <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900|RobotoDraft:400,100,300,500,700,900'>
 <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
 
-      <link rel="stylesheet" href="css/login.css">
+      <link rel="stylesheet" href="login.css">
 
 	  <style type="text/css">
 	  #buttn{
@@ -22,36 +22,6 @@
 </head>
 
 <body>
-<?php
-include("connection/connect.php"); //INCLUDE CONNECTION
-error_reporting(0); // hide undefine index errors
-session_start(); // temp sessions
-if(isset($_POST['submit']))   // if button is submit
-{
-	$username = $_POST['username'];  //fetch records from login form
-	$password = $_POST['password'];
-	
-	if(!empty($_POST["submit"]))   // if records were not empty
-     {
-	$loginquery ="SELECT * FROM users WHERE username='$username' && password='".md5($password)."'"; //selecting matching records
-	$result=mysqli_query($db, $loginquery); //executing
-	$row=mysqli_fetch_array($result);
-	
-	                        if(is_array($row))  // if matching records in the array & if everything is right
-								{
-                                    	$_SESSION["user_id"] = $row['u_id']; // put user id into temp session
-										 header("refresh:1;url=index.php"); // redirect to index.php page
-	                            } 
-							else
-							    {
-                                      	$message = "Invalid Username or Password!"; // throw error
-                                }
-	 }
-	
-	
-}
-?>
-  
 <!-- Form Mixin-->
 <!-- Input Mixin-->
 <!-- Button Mixin-->
