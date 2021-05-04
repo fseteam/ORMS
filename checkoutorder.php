e5a9ec2444cbb16ec83ff78e236d69d5f2fab19d
abdimak <abdirohman567024@gmail.com>
2021-05-04 07:16:25 -0800

    second commit

--- checkoutorder.php
+++ checkoutorder.php
@@ -31,3 +31,56 @@ else{
 }
 }
 ?>
+<!-- start: FOOTER -->
+            <footer class="footer">
+                <div class="container">
+                    <!-- top footer statrs -->
+                    <div class="row top-footer">
+                        <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
+                            <a href="#"> <img src="images/food-picky-logo.png" alt="Footer logo"> </a> <span>Order Delivery &amp; Take-Out </span> </div>
+                        <div class="col-xs-12 col-sm-2 about color-gray">
+                            <h5>About Us</h5>
+                            <ul>
+                                <li><a href="#">About us</a> </li>
+                                <li><a href="#">History</a> </li>
+                                <li><a href="#">Our Team</a> </li>
+                                <li><a href="#">We are hiring</a> </li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
+                            <h5>How it Works</h5>
+                            <ul>
+                                <li><a href="#">Enter your location</a> </li>
+                                <li><a href="#">Choose restaurant</a> </li>
+                                <li><a href="#">Choose meal</a> </li>
+                                <li><a href="#">Pay via credit card</a> </li>
+                                <li><a href="#">Wait for delivery</a> </li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-2 pages color-gray">
+                            <h5>Pages</h5>
+                            <ul>
+                                <li><a href="#">Search results page</a> </li>
+                                <li><a href="#">User Sing Up Page</a> </li>
+                                <li><a href="#">Pricing page</a> </li>
+                                <li><a href="#">Make order</a> </li>
+                                <li><a href="#">Add to cart</a> </li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-3 popular-locations color-gray">
+                            <h5>Popular locations</h5>
+                            <ul>
+                            <li><a href="#">Adama</a> </li>
+                                <li><a href="#">Addis Abeba</a> </li>
+                                <li><a href="#">Dire Dhawa</a> </li>
+                                <li><a href="#">Hawasa</a> </li>
+                                <li><a href="#">Gonder</a> </li>
+                                <li><a href="#">Semara</a> </li>
+                                <li><a href="#">Mekele</a> </li>
+                                <li><a href="#">Jijiga</a> </li>
+                                <li><a href="#">Harar</a> </li>
+                                <li><a href="#">Other where</a> </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- top footer ends -->
