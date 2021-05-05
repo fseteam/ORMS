3398928c8d3462d8bcf9e6ce86d4bbfa6e3f7b5e
abdimak <abdirohman567024@gmail.com>
2021-05-04 21:28:07 -0800

    first commit

---
+++ loginnew.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>login page</title>
+</head>
+<?php
+include("connection/connect.php"); //INCLUDE CONNECTION
+error_reporting(0); // hide undefine index errors
+session_start(); // temp sessions
+if(isset($_POST['submit']))   // if button is submit
+{
+	$username = $_POST['username'];  //fetch records from login form
+	$password = $_POST['password'];
+	
+	if(!empty($_POST["submit"]))   // if records were not empty
+     {
+$loginquery ="SELECT * FROM users WHERE username='$username' && password='".md5($password)."'"; //selecting matching records
+	$result=mysqli_query($db, $loginquery); //executing
+	$row=mysqli_fetch_array($result);
+	
+	                        if(is_array($row))  // if matching records in the array & if everything is right
+								{
+                                    	$_SESSION["user_id"] = $row['u_id']; // put user id into temp session
+										 header("refresh:1;url=index.php"); // redirect to index.php page
+	                            } 
+							else
+							    {
+                                      	$message = "Invalid Username or Password!"; // throw error
+                                }
+	 }
+}
