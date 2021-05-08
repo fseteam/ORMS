2f69489af538755cba4608ebfea26ac686ed0a4e
abdimak <abdirohman567024@gmail.com>
2021-05-07 21:41:52 -0800

    commiting

--- loginnew.php
+++ loginnew.php
@@ -31,3 +31,39 @@ $loginquery ="SELECT * FROM users WHERE username='$username' && password='".md5(
                                 }
 	 }
 }
+<!-- Form Mixin-->
+<!-- Input Mixin-->
+<!-- Button Mixin-->
+<!-- Pen Title-->
+<div class="pen-title">
+  <h1>Login Form</h1>
+</div>
+<!-- Form Module-->
+<div class="module form-module">
+  <div class="toggle">
+   
+  </div>
+  <div class="form">
+    <h2>Login to your account</h2>
+	  <span style="color:red;"><?php echo $message; ?></span> 
+   <span style="color:green;"><?php echo $success; ?></span>
+    <form action="" method="post">
+      <input type="text" placeholder="Username"  name="username"/>
+      <input type="password" placeholder="Password" name="password"/>
+      <input type="submit" id="buttn" name="submit" value="login" />
+    </form>
+  </div>
+  
+  <div class="cta">Not registered?<a href="registration.php" style="color:#f30;"> Create an account</a></div>
+</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+  
+
+   
+
+
+
+</body>
+
+</html>
