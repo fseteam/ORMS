3c210d434361a1a9275ccd87ec05fe285d7529a5
Ararso <arasoalisho2@gmial.com>
2021-05-14 18:22:01 +0300

    dishes file was created

---
+++ dishes.php
@@ -0,0 +1,254 @@
+<!-- start: FOOTER -->
+            <footer class="footer">
+            <div class="container">
+                <!-- top footer statrs -->
+                <div class="row top-footer">
+                    <div class="col-xs-12 col-sm-3 footer-logo-block color-gray">
+                        <a href="#"> <img src="images/food-picky-logo.png" alt="Footer logo"> </a> <span>Order Delivery &amp; Take-Out </span> </div>
+                    <div class="col-xs-12 col-sm-2 about color-gray">
+                        <h5>About Us</h5>
+                        <ul>
+                            <li><a href="#">About us</a> </li>
+                            <li><a href="#">History</a> </li>
+                            <li><a href="#">Our Team</a> </li>
+                            
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-2 how-it-works-links color-gray">
+                        <h5>How it Works</h5>
+                        <ul>
+                            <li><a href="#">Enter your location</a> </li>
+                            <li><a href="#">Choose restaurant</a> </li>
+                            <li><a href="#">Choose meal</a> </li>
+                            <li><a href="#">Pay via credit card or bank account</a> </li>
+                            <li><a href="#">Wait for delivery</a> </li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-2 pages color-gray">
+                        <h5>Pages</h5>
+                        <ul>
+                            <li><a href="#">Search results page</a> </li>
+                            <li><a href="#">User Sing Up Page</a> </li>
+                            <li><a href="#">Pricing page</a> </li>
+                            <li><a href="#">Make order</a> </li>
+                            <li><a href="#">Add to cart</a> </li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-3 popular-locations color-gray">
+                        <h5>Popular locations</h5>
+                        <ul>
+                            <li><a href="#">Adama</a> </li>
+                            <li><a href="#">Addis Abeba</a> </li>
+                            <li><a href="#">Dire Dhawa</a> </li>
+                            <li><a href="#">Hawasa</a> </li>
+                            <li><a href="#">Gonder</a> </li>
+                            <li><a href="#">Semara</a> </li>
+                            <li><a href="#">Mekele</a> </li>
+                            <li><a href="#">Jijiga</a> </li>
+                            <li><a href="#">Harar</a> </li>
+                            <li><a href="#">Other where</a> </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- top footer ends -->
+                <!-- bottom footer statrs -->
+                <div class="bottom-footer">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                            <h5>Payment Options</h5>
+                            <ul>
+                                <li>
+                                    <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 address color-gray">
+                            <h5>Address</h5>
+                            <p>Concept design of oline food order and deliveye,planned as restaurant directory</p>
+                            <h5>Phone: <a href="tel:+251934408928">+251 93 440 8928</a></h5> </div>
+                            
+                        <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                            <h5>Addition informations</h5>
+                            <p>Join the thousands of other restaurants who benefit from having their menus on TakeOff</p>
+                            <div class="social-media">
+                               <a href="facebook"> <i class="fa fa-facebook"></i></a>
+                               <a href="gmail"> <i class="fa fa-email"></i></a>
+                               <a href="website"> <i class="fa fa-google"></i></a>
+                               <a href="whatsapp"> <i class="fa fa-whatsapp"></i></a>
+                               <a href="telegram"> <i class="fa fa-telegram"></i></a>
+                               <a href="youtube"> <i class="fa fa-youtube"></i></a>
+                               <a href="instagram"> <i class="fa fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- bottom footer ends -->
+            </div>
+        </footer>
+                    <!-- bottom footer ends -->
+                </div>
+            </footer>
+            <!-- end:Footer -->
+        </div>
+        <!-- end:page wrapper -->
+    </div>
+    <!--/end:Site wrapper -->
+    <!-- Modal -->
+    <div class="modal fade" id="order-modal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
+                <div class="modal-body cart-addon">
+                    <div class="food-item white">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6 col-lg-6">
+                                <div class="item-img pull-left">
+                                    <a class="restaurant-logo pull-left" href="#"><img src="http://placehold.it/70x70" alt="Food logo"></a>
+                                </div>
+                                <!-- end:Logo -->
+                                <div class="rest-descr">
+                                    <h6><a href="#">Sandwich de Alegranza Grande Menü (28 - 30 cm.)</a></h6> </div>
+                                <!-- end:Description -->
+                            </div>
+                            <!-- end:col -->
+                            <div class="col-xs-6 col-sm-2 col-lg-2 text-xs-center"> <span class="price pull-left">$ 2.99</span></div>
+                            <div class="col-xs-6 col-sm-4 col-lg-4">
+                                <div class="row no-gutter">
+                                    <div class="col-xs-7">
+                                        <select class="form-control b-r-0" id="exampleSelect2">
+                                            <option>Size SM</option>
+                                            <option>Size LG</option>
+                                            <option>Size XL</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-xs-5">
+                                        <input class="form-control" type="number" value="0" id="quant-input-2"> </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end:row -->
+                    </div>
+                    <!-- end:Food item -->
+                    <div class="food-item">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6 col-lg-6">
+                                <div class="item-img pull-left">
+                                    <a class="restaurant-logo pull-left" href="#"><img src="http://placehold.it/70x70" alt="Food logo"></a>
+                                </div>
+                                <!-- end:Logo -->
+                                <div class="rest-descr">
+                                    <h6><a href="#">Sandwich de Alegranza Grande Menü (28 - 30 cm.)</a></h6> </div>
+                                <!-- end:Description -->
+                            </div>
+                            <!-- end:col -->
+                            <div class="col-xs-6 col-sm-2 col-lg-2 text-xs-center"> <span class="price pull-left">$ 2.49</span></div>
+                            <div class="col-xs-6 col-sm-4 col-lg-4">
+                                <div class="row no-gutter">
+                                    <div class="col-xs-7">
+                                        <select class="form-control b-r-0" id="exampleSelect3">
+                                            <option>Size SM</option>
+                                            <option>Size LG</option>
+                                            <option>Size XL</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-xs-5">
+                                        <input class="form-control" type="number" value="0" id="quant-input-3"> </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end:row -->
+                    </div>
+                    <!-- end:Food item -->
+                    <div class="food-item">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6 col-lg-6">
+                                <div class="item-img pull-left">
+                                    <a class="restaurant-logo pull-left" href="#"><img src="http://placehold.it/70x70" alt="Food logo"></a>
+                                </div>
+                                <!-- end:Logo -->
+                                <div class="rest-descr">
+                                    <h6><a href="#">Sandwich de Alegranza Grande Menü (28 - 30 cm.)</a></h6> </div>
+                                <!-- end:Description -->
+                            </div>
+                            <!-- end:col -->
+                            <div class="col-xs-6 col-sm-2 col-lg-2 text-xs-center"> <span class="price pull-left">$ 1.99</span></div>
+                            <div class="col-xs-6 col-sm-4 col-lg-4">
+                                <div class="row no-gutter">
+                                    <div class="col-xs-7">
+                                        <select class="form-control b-r-0" id="exampleSelect5">
+                                            <option>Size SM</option>
+                                            <option>Size LG</option>
+                                            <option>Size XL</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-xs-5">
+                                        <input class="form-control" type="number" value="0" id="quant-input-4"> </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end:row -->
+                    </div>
+                    <!-- end:Food item -->
+                    <div class="food-item">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6 col-lg-6">
+                                <div class="item-img pull-left">
+                                    <a class="restaurant-logo pull-left" href="#"><img src="http://placehold.it/70x70" alt="Food logo"></a>
+                                </div>
+                                <!-- end:Logo -->
+                                <div class="rest-descr">
+                                    <h6><a href="#">Sandwich de Alegranza Grande Menü (28 - 30 cm.)</a></h6> </div>
+                                <!-- end:Description -->
+                            </div>
+                            <!-- end:col -->
+                            <div class="col-xs-6 col-sm-2 col-lg-2 text-xs-center"> <span class="price pull-left">$ 3.15</span></div>
+                            <div class="col-xs-6 col-sm-4 col-lg-4">
+                                <div class="row no-gutter">
+                                    <div class="col-xs-7">
+                                        <select class="form-control b-r-0" id="exampleSelect6">
+                                            <option>Size SM</option>
+                                            <option>Size LG</option>
+                                            <option>Size XL</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-xs-5">
+                                        <input class="form-control" type="number" value="0" id="quant-input-5"> </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end:row -->
+                    </div>
+                    <!-- end:Food item -->
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn theme-btn">Add to cart</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
