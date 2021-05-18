ddd5c1c2c8a7758ed8087655032d9f6a77f3b243
Ararso <arasoalisho2@gmial.com>
2021-05-19 00:30:50 +0300

    content added

--- index.php
+++ index.php
@@ -31,16 +31,16 @@ session_start(); //start temp session until logout/browser closed
             <nav class="navbar navbar-dark">
                 <div class="container">
                     <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
-                    <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/food-picky-logo.png" alt=""> </a>
+                    <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/logo1.png" alt=""> </a>
                     <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
                         <ul class="nav navbar-nav">
                             <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
                             <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
                             
                            
-				<?php
+							<?php
 						if(empty($_SESSION["user_id"])) // if user is not login
-				{
+							{
 								echo '<li class="nav-item"><a href="login.php" class="nav-link active">login</a> </li>
 							  <li class="nav-item"><a href="registration.php" class="nav-link active">signup</a> </li>';
 							}
@@ -62,7 +62,7 @@ session_start(); //start temp session until logout/browser closed
             <!-- /.navbar -->
         </header>
         <!-- banner part starts -->
-        <section class="hero bg-image" data-image-src="images/img/main.jpeg">
+        <section class="hero bg-image" data-image-src="images/picture/burger.jfif">
             <div class="hero-inner">
                 <div class="container text-center hero-text font-white">
                     <h1>Welcome to Online Restaurant Management System</h1>
@@ -114,33 +114,34 @@ session_start(); //start temp session until logout/browser closed
 						<?php 
 						// fetch records from database to display popular first 3 dishes from table
 						$query_res= mysqli_query($db,"select * from dishes LIMIT 3"); 
-									      while($r=mysqli_fetch_array($query_res))
-										  {
-													
-						                       echo '  <div class="col-xs-12 col-sm-6 col-md-4 food-item">
-														<div class="food-item-wrap">
-															<div class="figure-wrap bg-image" data-image-src="admin/Res_img/dishes/'.$r['img'].'">
-																
-																<div class="rating pull-left"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star-o"></i> </div>
-																<div class="review pull-right"><a href="#">198 reviews</a> </div>
-															</div>
-															<div class="content">
-																<h5><a href="dishes.php?res_id='.$r['rs_id'].'">'.$r['title'].'</a></h5>
-																<div class="product-name">'.$r['slogan'].'</div>
-																<div class="price-btn-block"> <span class="price">$'.$r['price'].'</span> <a href="dishes.php?res_id='.$r['rs_id'].'" class="btn theme-btn-dash pull-right">Order Now</a> </div>
-                                                                <div class="ingredient"> <a href="#"> Ingredient </a> </div>
-															</div>
-															
-														</div>
-												</div>';
-													
-										  }
+                        while($r=mysqli_fetch_array($query_res))
+                        {
+                                
+                            echo '  <div class="col-xs-12 col-sm-6 col-md-4 food-item">
+                                    <div class="food-item-wrap">
+                                        <div class="figure-wrap bg-image" data-image-src="admin/Res_img/dishes/'.$r['img'].'">
+                                            
+                                            <div class="rating pull-left"> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star-o"></i> </div>
+                                            <div class="review pull-right"><a href="#">198 reviews</a> </div>
+                                        </div>
+                                        <div class="content">
+                                            <h5><a href="dishes.php?res_id='.$r['rs_id'].'">'.$r['title'].'</a></h5>
+                                            <div class="product-name">'.$r['slogan'].'</div>
+                                            <div class="price-btn-block"> <span class="price">birr '.$r['price'].'</span> <a href="dishes.php?res_id='.$r['rs_id'].'" class="btn theme-btn-dash pull-right">Order Now</a> </div>
+                                            
+                                            <button onclick="ingredient">Ingredient</button>
+                                            
+                                            
+                                        </div>
+                                        
+                                    </div>
+                            </div>';
+                                
+                        }
 						
 						
 						?>
-			
-					
-                 
+                       
                 </div>
             </div>
         </section>
@@ -207,7 +208,7 @@ session_start(); //start temp session until logout/browser closed
                 <div class="row">
                     <div class="col-sm-4">
                         <div class="title-block pull-left">
-                            <h4>Featured restaurants</h4> </div>
+                            <h4>Available restaurants</h4> </div>
                     </div>
                     <div class="col-sm-8">
                         <!-- restaurants filter nav starts -->
@@ -237,39 +238,39 @@ session_start(); //start temp session until logout/browser closed
 						
 						<?php  //fetching records from table and filter using html data-filter tag
 						$ress= mysqli_query($db,"select * from restaurant");  
-									      while($rows=mysqli_fetch_array($ress))
-										  {
-													// fetch records from res_category table according to catgory ID
-													$query= mysqli_query($db,"select * from res_category where c_id='".$rows['c_id']."' ");
-													 $rowss=mysqli_fetch_array($query);
-						
-													 echo ' <div class="col-xs-12 col-sm-12 col-md-6 single-restaurant all '.$rowss['c_name'].'">
-														<div class="restaurant-wrap">
-															<div class="row">
-																<div class="col-xs-12 col-sm-3 col-md-12 col-lg-3 text-xs-center">
-																	<a class="restaurant-logo" href="dishes.php?res_id='.$rows['rs_id'].'" > <img src="admin/Res_img/'.$rows['image'].'" alt="Restaurant logo"> </a>
-																</div>
-																<!--end:col -->
-																<div class="col-xs-12 col-sm-9 col-md-12 col-lg-9">
-																	<h5><a href="dishes.php?res_id='.$rows['rs_id'].'" >'.$rows['title'].'</a></h5> <span>'.$rows['address'].'</span>
-																	<div class="bottom-part">
-																		
-																		<div class="ratings"> <span>
-																				<i class="fa fa-star"></i>
-																				<i class="fa fa-star"></i>
-																				<i class="fa fa-star"></i>
-																				<i class="fa fa-star"></i>
-																				<i class="fa fa-star-o"></i>
-																			</span> (122) </div>
-																	</div>
-																</div>
-																<!-- end:col -->
-															</div>
-															<!-- end:row -->
-														</div>
-														<!--end:Restaurant wrap -->
-													</div>';
-										  }
+                            while($rows=mysqli_fetch_array($ress))
+                            {
+                                // fetch records from res_category table according to catgory ID
+                                $query= mysqli_query($db,"select * from res_category where c_id='".$rows['c_id']."' ");
+                                    $rowss=mysqli_fetch_array($query);
+
+                                    echo ' <div class="col-xs-12 col-sm-12 col-md-6 single-restaurant all '.$rowss['c_name'].'">
+                                    <div class="restaurant-wrap">
+                                        <div class="row">
+                                            <div class="col-xs-12 col-sm-3 col-md-12 col-lg-3 text-xs-center">
+                                                <a class="restaurant-logo" href="dishes.php?res_id='.$rows['rs_id'].'" > <img src="admin/Res_img/'.$rows['image'].'" alt="Restaurant logo"> </a>
+                                            </div>
+                                            <!--end:col -->
+                                            <div class="col-xs-12 col-sm-9 col-md-12 col-lg-9">
+                                                <h5><a href="dishes.php?res_id='.$rows['rs_id'].'" >'.$rows['title'].'</a></h5> <span>'.$rows['address'].'</span>
+                                                <div class="bottom-part">
+                                                    
+                                                    <div class="ratings"> <span>
+                                                            <i class="fa fa-star"></i>
+                                                            <i class="fa fa-star"></i>
+                                                            <i class="fa fa-star"></i>
+                                                            <i class="fa fa-star"></i>
+                                                            <i class="fa fa-star-o"></i>
+                                                        </span> (122) </div>
+                                                </div>
+                                            </div>
+                                            <!-- end:col -->
+                                        </div>
+                                        <!-- end:row -->
+                                    </div>
+                                    <!--end:Restaurant wrap -->
+                                </div>';
+                            }
 						
 						
 						?>
@@ -282,7 +283,8 @@ session_start(); //start temp session until logout/browser closed
                 <!-- restaurants listing ends -->
                
             </div>
-<!-- Featured restaurants ends -->
+        </section>
+        <!-- Featured restaurants ends -->
         
         <!-- start: FOOTER -->
         <footer class="footer">
@@ -387,6 +389,7 @@ session_start(); //start temp session until logout/browser closed
     
     <!-- Bootstrap core JavaScript
     ================================================== -->
+    <script src="js/jquery.js"></script>
     <script src="js/jquery.min.js"></script>
     <script src="js/tether.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
@@ -395,4 +398,6 @@ session_start(); //start temp session until logout/browser closed
     <script src="js/jquery.isotope.min.js"></script>
     <script src="js/headroom.js"></script>
     <script src="js/foodpicky.min.js"></script>
-</body>        </section>
+</body>
+
+</html>
